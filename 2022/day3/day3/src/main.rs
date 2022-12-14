57ec7479faad2acfed81f586e21fc9a5dd3479f5
MorganB <morganbrockman1998@gmail.com>
2022-12-14 12:42:59 -0800

    day3 part 2

--- 2022/day3/day3/src/main.rs
+++ 2022/day3/day3/src/main.rs
@@ -1,24 +1,26 @@
-use std::{io, collections::HashSet};
+use std::{collections::HashSet, io};
 
 fn main() {
     let mut sum = 0;
     let mut buffer = String::new();
-    loop {
-        io::stdin().read_line(&mut buffer).expect("input should be readable");
-        if buffer.trim().len() == 0 { break; } // Reached end of input
-        let rucksack: Vec<char> = buffer.trim().chars().collect();
-        let rucksack: Vec<&[char]> = rucksack.chunks(rucksack.len()/2).collect();
-        if rucksack.len() != 2 { panic!("rucksack should be even"); }
-        let set1: HashSet<&char> = rucksack[0].iter().collect();
-        let set2: HashSet<&char> = rucksack[1].iter().collect();
-        let mistakes: Vec<&&char> = set1.intersection(&set2).collect();
+    'prime_loop: loop {
+        let mut group = [HashSet::new(), HashSet::new(), HashSet::new()];
+        for i in 0..3 {
+            io::stdin().read_line(&mut buffer).expect("input should be readable");
+            if buffer.trim().len() == 0 { break 'prime_loop; } // Reached end of input
+            let rucksack: HashSet<char> = buffer.trim().chars().collect();
+            group[i] = rucksack;
+            buffer.clear();
+        }
+        let mistakes = group[0]
+            .intersection(&group[1]).copied().collect::<HashSet<char>>()
+            .intersection(&group[2]).copied().collect::<Vec<char>>();
         if mistakes.len() != 1 { panic!("rucksack should have only one mistake, not {}", mistakes.len()); }
-        let mistake = **mistakes[0];
+        let mistake = mistakes[0];
         match mistake.is_uppercase() {
             true => sum = sum + mistake as u32 - 38,
             false => sum = sum + mistake as u32 - 96
         }
-        buffer.clear();
     }
     println!("Sum: {}", sum);
 }

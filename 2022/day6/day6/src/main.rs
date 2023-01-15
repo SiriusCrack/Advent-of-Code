2c070accfae43b8ee87dd00741cc4a65c5c9403c
Morgan Brockman <morganbrockman1998@gmail.com>
2023-01-14 17:04:45 -0800

    day6 part 1 complete

--- 2022/day6/day6/src/main.rs
+++ 2022/day6/day6/src/main.rs
@@ -1,3 +1,26 @@
+use std::{io, collections::HashSet};
+
 fn main() {
-    println!("Hello, world!");
+    let datastream = populate_datasastream();
+    let marker = find_marker(datastream);
+    println!("{}", marker);
+}
+
+fn populate_datasastream() -> Vec<char> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let datastream: Vec<char> = buffer.chars().collect();
+    datastream
+}
+
+fn find_marker(datastream: Vec<char>) -> usize {
+    let mut marker = 0;
+    for (i, window) in datastream.windows(4).enumerate() {
+        let set: HashSet<&char> = window.iter().collect();
+        if set.len() == 4 {
+            marker = i+4;
+            break;
+        }
+    }
+    marker
 }
